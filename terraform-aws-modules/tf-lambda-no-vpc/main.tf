e3dc516f881faa801daeda214c7bdc6e18ebb715
Zied KALLEL <kallelzied@live.com>
2019-05-01 04:26:19 +0200

    added lambda without vpc module (#4)

---
+++ terraform-aws-modules/tf-lambda-no-vpc/main.tf
@@ -0,0 +1,13 @@
+resource "aws_lambda_function" "lambda-function" {
+  filename      = "${var.lambda-function-zip-path}"
+  function_name = "${var.lambda-function-name}"
+  handler       = "${var.lambda-function-entrypoint}"
+  role          = "${var.lambda-function-role-arn}"
+  description   = "${var.lambda-function-description}"
+  memory_size   = "${var.lambda-function-memory-size}"
+  runtime       = "${var.lambda-function-runtime}"
+  timeout       = "${var.lambda-function-timeout}"
+  publish       = "${var.lambda-function-publish}"
+
+  source_code_hash = "${base64sha256(file(var.lambda-function-zip-path))}"
+}
---
+++ terraform-aws-modules/tf-lambda-no-vpc/variables.tf
@@ -0,0 +1,40 @@
+variable "lambda-function-zip-path" {
+  description = "Lambda function zip source path"
+}
+
+variable "lambda-function-name" {
+  description = "Lambda function name"
+}
+
+variable "lambda-function-entrypoint" {
+  description = "The handler to execute as an entrypoint of the lambda function"
+}
+
+variable "lambda-function-role-arn" {
+  description = "The role arn to associate with the lambda function"
+}
+
+variable "lambda-function-description" {
+  description = "The description of the lambda function"
+  default = ""
+}
+
+variable "lambda-function-memory-size" {
+  description = "The amount of memory to allocate for the lambda function"
+  default     = "128"
+}
+
+variable "lambda-function-runtime" {
+  description = "The runtime of the lambda function"
+  default = "python3.7"
+}
+
+variable "lambda-function-timeout" {
+  description = "The lambda function timeout"
+  default     = 30
+}
+
+variable "lambda-function-publish" {
+  description = "Versionning changes of the lambda function"
+  default     = "false"
+}
